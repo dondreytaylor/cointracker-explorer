360a2dab0f307860fc5f691564804b9a44e3fb1b
Dondrey Taylor <dondrey.taylor@gmail.com>
2023-07-11 01:29:59 -0400

    Added bindings to address page

--- frontend/site/src/modules/module-site/pages/page-address/page-address.component.ts
+++ frontend/site/src/modules/module-site/pages/page-address/page-address.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as services from '../../../module-shared/services/services.barrel'
+import { ActivatedRoute } from '@angular/router';
+import { BTCUnits } from '../../../module-shared/constants/BTCUnits'
+
 
 @Component({
   selector: 'app-page-address',
@@ -8,12 +11,249 @@ import * as services from '../../../module-shared/services/services.barrel'
 })
 export class PageAddressComponent implements OnInit {
 
-  constructor(private blockchain:services.BlockchainCOM) { }
+  isFetching:boolean = true; 
+  BTCUnits = BTCUnits
+
+  addressData:any = {
+    "hash160": "f74f591a65c8c5ff5f3804815c8cc56943df5d4f",
+    "address": "1PYexakYRvWnzYa21kPWqbXgwPvLjnttMp",
+    "n_tx": 2,
+    "n_unredeemed": 0,
+    "total_received": 99630000,
+    "total_sent": 99630000,
+    "final_balance": 0,
+    "txs": [
+      {
+        "hash": "902e9138a6743ff3c2213a9f253d81c38bc945308d2d8cfca3230b01911c9dbb",
+        "ver": 1,
+        "vin_sz": 4,
+        "vout_sz": 2,
+        "size": 798,
+        "weight": 3192,
+        "fee": 500000,
+        "relayed_by": "0.0.0.0",
+        "lock_time": 0,
+        "tx_index": 6601070832787812,
+        "double_spend": false,
+        "time": 1382362353,
+        "block_index": 265075,
+        "block_height": 265075,
+        "inputs": [
+          {
+            "sequence": 4294967295,
+            "witness": "",
+            "script": "483045022100e67dec21d55924bc5dfb526658d2b6acea7ea8590ffb36c0618ebc08dec3365f02202a29f3a7bf96daa3408a2bc1349e9c36971bfa176f286aa51c834caf1ef0f8160141043a5ddb3785ac36d7e0253aa34faa2a6991ad48e8ef33ab97ca7d0b949ed5ee8398866fbd5c51c4f52bfa869fc868269a7ee9dd00995a152d13fad282524a693f",
+            "index": 0,
+            "prev_out": {
+              "addr": "1PYexakYRvWnzYa21kPWqbXgwPvLjnttMp",
+              "n": 0,
+              "script": "76a914f74f591a65c8c5ff5f3804815c8cc56943df5d4f88ac",
+              "spending_outpoints": [
+                {
+                  "n": 0,
+                  "tx_index": 6601070832787812
+                }
+              ],
+              "spent": true,
+              "tx_index": 7534748685607153,
+              "type": 0,
+              "value": 99630000
+            }
+          },
+          {
+            "sequence": 4294967295,
+            "witness": "",
+            "script": "483045022100ec511a54b552aed6d4fe67ca5936950e2c863d7d98319f2eb2012f138597c99102202cd3ef76d70a72cfc6988155f5acce0828542d9d138af42e4057267c226cdc8f014104d2c3879fdfb0b7ceb4da11293850a46dfbfe95cbe49fcef48b574b5c09c0523a73caad1884f9385a19e43e8838f22de0fc9edb527126d7eff7e5bc3b2e187ab6",
+            "index": 1,
+            "prev_out": {
+              "addr": "13Z1TRXtJ1K7FajMB2TkbZu4FrkMbBnhbw",
+              "n": 1,
+              "script": "76a9141bfdf1507a8e871471ceb0f2a5e94d9e0a11cbd088ac",
+              "spending_outpoints": [
+                {
+                  "n": 1,
+                  "tx_index": 6601070832787812
+                }
+              ],
+              "spent": true,
+              "tx_index": 4491888672498934,
+              "type": 0,
+              "value": 49680000
+            }
+          },
+          {
+            "sequence": 4294967295,
+            "witness": "",
+            "script": "473044022026450fb2991fe714a53cb2f9d588887acba7d5afe2b680288290770de0c538fe0220781d60a27ffd30deb8bb8fb3ecdadad6eda43f74105b91f249e66621a1e4e7700141043f412f1a0d01114a7c16fff30e2eff567d98f2ed7ccac566df68e068198cebacc3286fbf58e2b1a5d7d6eca6b944982894f2b5b6bb395decfd2b49474a01d74b",
+            "index": 2,
+            "prev_out": {
+              "addr": "1HgKEqDcB2pWtk1gPqzD55bAQUfcxa3tc6",
+              "n": 0,
+              "script": "76a914b6f175b2c990756b89978b70804bb78233d642df88ac",
+              "spending_outpoints": [
+                {
+                  "n": 2,
+                  "tx_index": 6601070832787812
+                }
+              ],
+              "spent": true,
+              "tx_index": 1020003707348624,
+              "type": 0,
+              "value": 924306491
+            }
+          },
+          {
+            "sequence": 4294967295,
+            "witness": "",
+            "script": "493046022100f3ed8a8bb4e9b8e173c1821919200d7cc8bed8dd16df1938a65a0ad31aa02037022100f174fe2844b7eceb7b814a80c6e9c646008db9003d2b595d7de69f3a99c86aa40141045c321c89e5a89054666bd60a4905f4961ad450bf8d667cf39852d77301310ecd09aca2c625dc23999df1baa1796d887d3d3e22966b3e2fc12fc6b5e8806928fe",
+            "index": 3,
+            "prev_out": {
+              "addr": "1NRvcbXc3Psmbnoh6FNptYnYZZxncdVaWj",
+              "n": 1,
+              "script": "76a914eb116107266e5425157e546e7073472cbf2fb14b88ac",
+              "spending_outpoints": [
+                {
+                  "n": 3,
+                  "tx_index": 6601070832787812
+                }
+              ],
+              "spent": true,
+              "tx_index": 5353861323135759,
+              "type": 0,
+              "value": 19160000
+            }
+          }
+        ],
+        "out": [
+          {
+            "type": 0,
+            "spent": true,
+            "value": 500000000,
+            "spending_outpoints": [
+              {
+                "tx_index": 7313812202725718,
+                "n": 0
+              }
+            ],
+            "n": 0,
+            "tx_index": 6601070832787812,
+            "script": "76a91453cf3d60864b8f7cd2862bc4684f4168136d1d7688ac",
+            "addr": "18e9MCAVL6qudhrP2BDpac9FJ5hmyGfm8K"
+          },
+          {
+            "type": 0,
+            "spent": true,
+            "value": 592276491,
+            "spending_outpoints": [
+              {
+                "tx_index": 8282146667311660,
+                "n": 4
+              }
+            ],
+            "n": 1,
+            "tx_index": 6601070832787812,
+            "script": "76a914d1cc186845a1d4e0435b95b2642e77c7166bf24788ac",
+            "addr": "1L8JitMh3Jerstd915LZRZ6oms5X5uxodq"
+          }
+        ],
+        "result": -99630000,
+        "balance": 0
+      },
+      {
+        "hash": "844dde700fffed3d0d70e910bfb127db3bbabab29f451d6cd4880705178326d6",
+        "ver": 1,
+        "vin_sz": 1,
+        "vout_sz": 2,
+        "size": 258,
+        "weight": 1032,
+        "fee": 50000,
+        "relayed_by": "0.0.0.0",
+        "lock_time": 0,
+        "tx_index": 7534748685607153,
+        "double_spend": false,
+        "time": 1382352016,
+        "block_index": 265049,
+        "block_height": 265049,
+        "inputs": [
+          {
+            "sequence": 4294967295,
+            "witness": "",
+            "script": "483045022069de1ccc093d89858e5037ff63759b7161149e43c0e5c431e46fc80e9a511407022100be6104c1572c5831c9cc3bb57cf84490b543aa4423153616a8c99168fae9838e014104f0f86fa57c424deb160d0fc7693f13fce5ed6542c29483c51953e4fa87ebf247487ed79b1ddcf3de66b182217fcaf3fcef3fcb44737eb93b1fcb8927ebecea26",
+            "index": 0,
+            "prev_out": {
+              "addr": "1FwYmGEjXhMtxpWDpUXwLx7ndLNfFQncKq",
+              "n": 0,
+              "script": "76a914a3e2bcc9a5f776112497a32b05f4b9e5b2405ed988ac",
+              "spending_outpoints": [
+                {
+                  "n": 0,
+                  "tx_index": 7534748685607153
+                }
+              ],
+              "spent": true,
+              "tx_index": 2815046584088590,
+              "type": 0,
+              "value": 100000000
+            }
+          }
+        ],
+        "out": [
+          {
+            "type": 0,
+            "spent": true,
+            "value": 99630000,
+            "spending_outpoints": [
+              {
+                "tx_index": 6601070832787812,
+                "n": 0
+              }
+            ],
+            "n": 0,
+            "tx_index": 7534748685607153,
+            "script": "76a914f74f591a65c8c5ff5f3804815c8cc56943df5d4f88ac",
+            "addr": "1PYexakYRvWnzYa21kPWqbXgwPvLjnttMp"
+          },
+          {
+            "type": 0,
+            "spent": true,
+            "value": 320000,
+            "spending_outpoints": [
+              {
+                "tx_index": 3571305379615148,
+                "n": 52
+              }
+            ],
+            "n": 1,
+            "tx_index": 7534748685607153,
+            "script": "76a914ee198b241239940a3a049c86288ff8962d29018d88ac",
+            "addr": "1NhxRT8N6dPVoFRPb1TMzLqxscLVccTFD3"
+          }
+        ],
+        "result": 99630000,
+        "balance": 99630000
+      }
+    ]
+  };
+  
+  constructor(private blockchain:services.BlockchainCOM, private router: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.blockchain.getBTCAddress("1FwYmGEjXhMtxpWDpUXwLx7ndLNfFQncKq").pipe().subscribe(data=> {
-      console.log(data); 
+    this.isFetching = true;
+    this.router.queryParams.pipe().subscribe(queryParams=> {
+      if (queryParams.addr != undefined) {
+        //this.blockchain.getBTCAddress(queryParams.addr).pipe().subscribe(data=> {
+         //   this.addressData = data;
+            this.isFetching = false;
+        //})
+      }
+      else {
+        this.isFetching = true;
+      }
     })
+  }
+
+  getDate(time:number) {
+    return (new Date(time * 1000)).toString();
   }
 
 }
